161c73da47e9756232429a536a5f261c758f7491
zhufengzhufeng <894918097@qq.com>
2016-11-03 18:19:33 +0800

    5中服务

---
+++ fourth/this_test.js
@@ -0,0 +1,8 @@
+function A() {
+    this['+'] = function () {return a+b;};
+    this['-'] = function () {return a-b;};
+    this['*'] = function () {return a*b;};
+    this['/'] = function () {return a/b;};
+}
+var a = newonsole.log(a); A();
+c
